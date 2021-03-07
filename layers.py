c2b080a76fe6f1c6204bd75d831cff970923badc
olekkr <olekkr@outlook.com>
2021-03-07 18:01:27 +0100

    things sorta work, needs verification

--- layers.py
+++ layers.py
@@ -1,87 +1,115 @@
 import numpy as np
 
 
-
 # Dense layer
 class Layer_Dense:
 
     # Layer initialization
-    def __init__(self, n_inputs, n_neurons):
-        # Initialize weights and biases
-        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
+    def __init__(self, ninputs, nneurons):
+        (self.n_inputs, self.n_neurons) = ninputs, nneurons
+        
+
+    # Initialize weights and biases
+    def init(self):
+        self.weights = []
+        for i in range(self.model.batchsize):
+            self.weights.append(0.01 * np.random.randn(self.n_inputs, self.n_neurons))
+        self.biases = (np.zeros((self.model.batchsize ,1, self.n_neurons)))
+        #print(self.weights, self.biases, "layer params")
 
     # Forward pass
     def forward(self, inputs):
-
         # Calculate output values from inputs, weights and biases
-        self.output = np.dot(inputs, self.weights) + self.biases
+        self.output = []
+        #print(inputs, "layer input")
+        for (inputt, weight, bias) in zip(inputs, self.weights, self.biases):
+            self.output.append(np.dot(inputt, weight) + bias)
+            #print(inputt.shape, weight.shape. bias)
+            #print(inputt.shape, weight.shape, bias.shape)
+        print(self.output, "layer out")
         self.fwd.forward(self.output)
         
 
 
 # ReLU activation
 class Activation_ReLU:
+
+    def __init__(self):
+        pass
+
+    def init(self):
+        pass
     # Forward pass
     def forward(self, inputs):
-
         # Calculate output values from inputs
         self.output = np.maximum(0, inputs)
         self.fwd.forward(self.output)
-        
+        #print(self.output, "relu out")
 
 
 # Softmax activation
 class Activation_Softmax:
+    def init(self):
+        pass
+
     # Forward pass
     def forward(self, inputs):
-
+        self.output = []
         # Get unnormalized probabilities
-        exp_values = np.exp(inputs - np.max(inputs, axis=1,
-                                            keepdims=True))
-        # Normalize them for each sample
-        probabilities = exp_values / np.sum(exp_values, axis=1,
-                                            keepdims=True)
-        self.output = probabilities
+        for input in inputs:
+            exp_values = np.exp(input - np.max(input, axis=1,
+                                                keepdims=True))
+            # Normalize them for each sample
+            probabilities = exp_values / np.sum(exp_values, axis=1,
+                                                keepdims=True)
+            self.output.append(probabilities)
+        
+        print(self.output, 'soft out')
         self.fwd.forward(self.output)
-        #print(self.output)
+    
+    
 
 
 # Cross-entropy loss
 class Loss_CategoricalCrossentropy:
 
+    def init(self):
+        pass
     # Forward pass
     def forward(self, y_pred):
+        self.output = []
 
-        # for the sake of testing change later
-        y_true = [0] * len(y_pred[0])
-        y_true[next(self.model.y_true)[0]] = 1
-        #print(y_true)
+        for pred in y_pred:
+            # for the sake of testing change later
+            y_true = [0] * len(pred[0])
+            test = (next(self.model.y_true))[0]
+            y_true[test] = 1
+            # print(y_true)
 
-        # Number of samples in a batch
-        samples = len(y_pred)
+            # Number of samples in a batch
+            samples = len(pred)
 
-        # Clip data to prevent division by 0
-        # Clip both sides to not drag mean towards any value
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+            # Clip data to prevent division by 0
+            # Clip both sides to not drag mean towards any value
+            y_pred_clipped = np.clip(pred, 1e-7, 1 - 1e-7)
 
-        # Probabilities for target values -
-        # only if categorical labels
-        # if len(y_true.shape) == 1:
-        # correct_confidences = y_pred_clipped[
-        # range(samples),
-        #    y_true
-        # ]
+            # Probabilities for target values -
+            # only if categorical labels
+            # if len(y_true.shape) == 1:
+            # correct_confidences = y_pred_clipped[
+            # range(samples),
+            #    y_true
+            # ]
 
-        # Mask values - only for one-hot encoded labels
-        # elif len(y_true.shape) == 2:
-        correct_confidences = np.sum(
-            y_pred_clipped*y_true,
-            axis=1
-        )
+            # Mask values - only for one-hot encoded labels
+            # elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true,
+                axis=1
+            )
 
-        # Losses
-        negative_log_likelihoods = -np.log(correct_confidences)
-        self.output = negative_log_likelihoods
+            # Losses
+            negative_log_likelihoods = -np.log(correct_confidences)
+            self.output.append(negative_log_likelihoods)
 
-        #print(self.output)
+        print(self.output, "loss")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from matplotlib import pyplot
-import mnist
+#import mnist
 import pickle
 
 from layers import *
@@ -37,22 +37,27 @@ class Model:
         #set fwd and back
         for l in self.layers:
             l.model = self
+            l.init()
         
         for idx, l in enumerate(self.layers):
-            if idx != len(layers)-1:
-                l.fwd = layers[idx + 1]
+            if idx != len(self.layers)-1:
+                l.fwd = self.layers[idx + 1]
             if idx != 0:
-                l.bck = layers[idx - 1]
-    
+                l.bck = self.layers[idx - 1]
+
+
     def XGen (self):
+        yield [[1,2,3],[1,2,3],[1,2,3]]
         gen = (s for s in trainData[0])
         for _ in gen:
             output = []
             for _ in range(self.batchsize):
                 output.append(np.array(next(gen)).ravel())
-            yield output
+            #yield output
+        
 
     def YGen (self):
+        yield [2,0,0]
         gen = (s for s in trainData[1])
         for _ in gen:
             output = []
@@ -61,18 +66,22 @@ class Model:
             yield output
     
     def forward(self):
-        input = next(self.inputs)
-        self.layers[0].forward(np.array(input))
+        input = np.array(next(self.inputs))
+        self.layers[0].forward(input)
+
         print(self.layers)
         #last = self.layers[0]
         #for item in self.layers[1:]:
         #    item.forward(last.output)
         #    last = item
 
+modeltest1 = Model([Layer_Dense(3, 4), Activation_Softmax(), Loss_CategoricalCrossentropy()],3) ## , Activation_Softmax()
+modeltest1.forward()
 
-layers = [Layer_Dense(28**2, 64), Activation_ReLU(), Layer_Dense(64, 10),  Activation_Softmax(), Loss_CategoricalCrossentropy()]
 
-model = Model(layers,1)
+#layers = [Layer_Dense(28**2, 64), Activation_ReLU(), Layer_Dense(64, 10),  Activation_ReLU(), Loss_CategoricalCrossentropy()]
 
-model.forward()
+#model = Model(layers,1)
+
+#model.forward()
 
