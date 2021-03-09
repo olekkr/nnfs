9a6d985166ed9f6900e6db60eece31c73f1ffe70
olekkr <olekkr@outlook.com>
2021-03-09 13:50:00 +0100

    hellyea nn is born

--- layers.py
+++ layers.py
@@ -7,28 +7,59 @@ class Layer_Dense:
     # Layer initialization
     def __init__(self, ninputs, nneurons):
         (self.n_inputs, self.n_neurons) = ninputs, nneurons
-        
 
     # Initialize weights and biases
+
     def init(self):
         self.weights = []
         for i in range(self.model.batchsize):
-            self.weights.append(0.01 * np.random.randn(self.n_inputs, self.n_neurons))
-        self.biases = (np.zeros((self.model.batchsize ,1, self.n_neurons)))
-        #print(self.weights, self.biases, "layer params")
+            self.weights.append(
+                0.01 * np.random.randn(self.n_inputs, self.n_neurons))
+        self.biases = (np.zeros((self.model.batchsize, 1, self.n_neurons)))
 
     # Forward pass
     def forward(self, inputs):
         # Calculate output values from inputs, weights and biases
+        self.inputs = inputs
         self.output = []
-        #print(inputs, "layer input")
         for (inputt, weight, bias) in zip(inputs, self.weights, self.biases):
             self.output.append(np.dot(inputt, weight) + bias)
-            #print(inputt.shape, weight.shape. bias)
-            #print(inputt.shape, weight.shape, bias.shape)
-        print(self.output, "layer out")
         self.fwd.forward(self.output)
+
+    def backward(self):
+        print(self)
         
+        #print(np.array(self.weights).shape, np.array(self.inputs).shape, self.biases, "params")
+
+        #dL/dw 
+        #for each elem in batch
+        dLdw = []
+        for db , ib in zip(self.fwd.derivatives, self.inputs):
+            #new in batch
+            new = []
+            for i in ib:
+                #ds = np.multiply(ds, i)
+                new.append(i*db[0])
+            dLdw.append(new)
+        #print(np.array(dLdw).shape, np.array(self.weights).shape, "testtestest")
+
+
+        #dL/db
+        dLdb = self.fwd.derivatives
+
+        
+        #dL/da is w * dL/dz
+        self.derivatives = self.weights * self.fwd.derivatives
+        
+        self.weights -= np.array(dLdw) * self.model.learningRate
+        self.biases -= np.array(dLdb) * self.model.learningRate
+
+        #print(dLdb, self.biases.shape)
+
+        if(not self.isStart):
+            self.bck.backward()
+
+
 
 
 # ReLU activation
@@ -40,12 +71,18 @@ class Activation_ReLU:
     def init(self):
         pass
     # Forward pass
+
     def forward(self, inputs):
         # Calculate output values from inputs
         self.output = np.maximum(0, inputs)
-        self.fwd.forward(self.output)
         #print(self.output, "relu out")
-
+        self.fwd.forward(self.output)
+    
+    def backward(self):
+        print(self)
+        self.derivatives = np.clip(self.output, 0, 1)
+        self.bck.backward()
+        pass
 
 # Softmax activation
 class Activation_Softmax:
@@ -54,62 +91,82 @@ class Activation_Softmax:
 
     # Forward pass
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = []
         # Get unnormalized probabilities
         for input in inputs:
             exp_values = np.exp(input - np.max(input, axis=1,
-                                                keepdims=True))
+                                               keepdims=True))
             # Normalize them for each sample
             probabilities = exp_values / np.sum(exp_values, axis=1,
                                                 keepdims=True)
             self.output.append(probabilities)
-        
-        print(self.output, 'soft out')
+
+        #print(self.output, 'soft out')
         self.fwd.forward(self.output)
-    
-    
+
+    def backward(self):
+        print(self)
+        # do for each elem in batch
+        # da(x)/d(notx)
+        # will be calculate dL/dz for each
+        self.derivatives = self.output
+        for (d, y) in zip(self.derivatives, self.fwd.y_trues):
+            #print(d,y, "howdy")
+            d[0][y] = d[0][y] - d[0][y]**2       
+        self.derivatives = np.multiply(self.derivatives, self.fwd.derivatives) 
+        #print(self.derivatives, self.fwd.y_trues, "der, trues")
+        self.bck.backward()
 
 
-# Cross-entropy loss
 class Loss_CategoricalCrossentropy:
 
     def init(self):
         pass
     # Forward pass
+
     def forward(self, y_pred):
+        self.y_pred = y_pred
+        self.y_trues = next(self.model.y_true)
+        self.trueVects = []
         self.output = []
 
-        for pred in y_pred:
-            # for the sake of testing change later
-            y_true = [0] * len(pred[0])
-            test = (next(self.model.y_true))[0]
-            y_true[test] = 1
-            # print(y_true)
+        nOutputs = len(y_pred[0][0])
+
+        for pred, true in zip(self.y_pred, self.y_trues):
+            # create onehot vector
+            trueVect = [0] * nOutputs
+            trueVect[true] = 1
+
+            # appends truevect for future use
+            self.trueVects.append(np.clip((trueVect), 1e-7, 1 - 1e-7))
 
             # Number of samples in a batch
-            samples = len(pred)
 
             # Clip data to prevent division by 0
             # Clip both sides to not drag mean towards any value
             y_pred_clipped = np.clip(pred, 1e-7, 1 - 1e-7)
 
-            # Probabilities for target values -
-            # only if categorical labels
-            # if len(y_true.shape) == 1:
-            # correct_confidences = y_pred_clipped[
-            # range(samples),
-            #    y_true
-            # ]
-
             # Mask values - only for one-hot encoded labels
             # elif len(y_true.shape) == 2:
             correct_confidences = np.sum(
-                y_pred_clipped * y_true,
+                y_pred_clipped * trueVect,
                 axis=1
             )
 
             # Losses
             negative_log_likelihoods = -np.log(correct_confidences)
             self.output.append(negative_log_likelihoods)
-
         print(self.output, "loss")
+        print(y_pred)
+        print([np.argmax(a) for a in y_pred], self.y_trues)
+
+    def backward(self):
+        print(self)
+        # da(x)/dn
+        # will be calculate dL/dz for each
+        self.derivatives = []
+        for (p, y) in zip(self.y_pred, self.trueVects):
+            self.derivatives.append(-y/p)
+            #print(self.derivatives, "der")
+        self.bck.backward()
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import numpy as np
 from matplotlib import pyplot
-#import mnist
+#import mnist 
 import pickle
+
+#///https://github.com/alexmeinhold/mnist-parser
+#https://github.com/olekkr/nnfs
 
 from layers import *
 
@@ -21,6 +24,7 @@ with open("./pickle", 'rb') as f:
 
 #pyplot.imshow(trainData[0][9])
 #pyplot.show()
+#print(trainData[1][0:100])
 
 
 class Model:
@@ -33,50 +37,61 @@ class Model:
 
         self.count = 1 
 
+        self.learningRate = 0.01
+
         self.layers = layerList
+
         #set fwd and back
         for l in self.layers:
             l.model = self
             l.init()
+            l.isStart = False  
+        
+        self.layers[0].isStart = True
         
         for idx, l in enumerate(self.layers):
             if idx != len(self.layers)-1:
                 l.fwd = self.layers[idx + 1]
-            if idx != 0:
+            if idx > 0:
                 l.bck = self.layers[idx - 1]
 
 
     def XGen (self):
-        yield [[1,2,3],[1,2,3],[1,2,3]]
         gen = (s for s in trainData[0])
         for _ in gen:
             output = []
             for _ in range(self.batchsize):
                 output.append(np.array(next(gen)).ravel())
-            #yield output
+            yield output
+            yield output
+            yield output
         
 
     def YGen (self):
-        yield [2,0,0]
         gen = (s for s in trainData[1])
-        for _ in gen:
+        for _ in trainData[1]:
             output = []
             for _ in range(self.batchsize):
-                output.append(int(next(gen)))
+                y = int(next(gen))
+                output.append(y)
+            yield output
+            yield output
             yield output
     
     def forward(self):
         input = np.array(next(self.inputs))
         self.layers[0].forward(input)
-
         print(self.layers)
-        #last = self.layers[0]
-        #for item in self.layers[1:]:
-        #    item.forward(last.output)
-        #    last = item
 
-modeltest1 = Model([Layer_Dense(3, 4), Activation_Softmax(), Loss_CategoricalCrossentropy()],3) ## , Activation_Softmax()
+    def backward(self):
+        self.layers[-1].backward()
+
+
+modeltest1 = Model([Layer_Dense(28**2, 10), Activation_ReLU(), Layer_Dense(10, 10), Activation_Softmax(), Loss_CategoricalCrossentropy()], 3) ## , Activation_Softmax()
 modeltest1.forward()
+modeltest1.backward()
+modeltest1.forward()
+modeltest1.backward()
 
 
 #layers = [Layer_Dense(28**2, 64), Activation_ReLU(), Layer_Dense(64, 10),  Activation_ReLU(), Loss_CategoricalCrossentropy()]
